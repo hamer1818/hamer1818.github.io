98857d4575d0472aac930b8a4d176720d83ed32c
hamer1818 <hortatepe2002@gmail.com>
2022-07-16 22:04:48 +0300

    a

--- assets/js/email.js
+++
@@ -1,9 +0,0 @@
-//create database
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'email'
-});
-connection.connect();
