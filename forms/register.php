a1a15895e14af29935d81aa2d2132490b7f03e26
hamer1818 <hortatepe2002@gmail.com>
2022-07-16 23:12:57 +0300

    a

---
+++ forms/register.php
@@ -0,0 +1,15 @@
+<?php
+
+  $username =$_POST['username'];
+  $password =$_POST['password'];
+  $email =$_POST['email'];
+  echo $username;
+  echo "<br />";
+  echo $password;
+  echo "<br />";
+  echo $email;
+
+
+
+  
+?>
