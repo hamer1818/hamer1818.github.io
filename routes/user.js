6a26930b24495887a250ea684c26e86b673b8daf
hamer1818 <47240233+hamer1818@users.noreply.github.com>
2022-09-21 16:16:25 +0300

    backend
    websitesi için backend kısmını geliştirmek için yeni çalımalara başladım

---
+++ routes/user.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const path = require("path");
+
+
+
+
+router.get("/", (req, res, next) => {
+    res.render('index');
+  });
+
+  module.exports = router;
